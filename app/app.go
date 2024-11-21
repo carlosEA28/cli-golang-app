bb26b6eeb3b1e949e008099cede6c5b8b4b0f6d1
carlosEA28 <carloseaduarte28@gmail.com>
2024-11-21 09:53:48 -0300

    adiciona funcao buscar ip

--- app/app.go
+++ app/app.go
@@ -1,13 +1,47 @@
 package app
 
-import "github.com/urfave/cli"
+import (
+	"fmt"
+	"log"
+	"net"
 
-//Its going to return the cli app ready to be executed
+	"github.com/urfave/cli"
+)
+
+// Its going to return the cli app ready to be executed
 func Gerar() *cli.App {
 
 	app := cli.NewApp()
 	app.Name = "Aplicação cli"
 	app.Usage = "Busca ips e nomes de servidor"
 
+	app.Commands = []cli.Command{
+		{
+			Name:  "ip",
+			Usage: "Busca ips de endereços",
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name:  "host",
+					Value: "devbook.com.br",
+				},
+			},
+			Action: BuscarIps,
+		},
+	}
+
 	return app
+}
+
+func BuscarIps(c *cli.Context) {
+	host := c.String("host")
+
+	ips, erro := net.LookupIP(host)
+
+	if erro != nil {
+		log.Fatal(erro)
+	}
+
+	for _, ip := range ips {
+		fmt.Println(ip)
+	}
 }
--- main.go
+++ main.go
@@ -2,13 +2,11 @@ package main
 
 import (
 	"cli/app"
-	"fmt"
 	"log"
 	"os"
 )
 
 func main() {
-	fmt.Println("Ponto de partida")
 
 	aplicação := app.Gerar()
 	erro := aplicação.Run(os.Args)
